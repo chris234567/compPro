064eea8261c2701cb7743df0e51fe68a6d7d46fb
chris <christoph-stade@outlook.de>
2021-09-19 11:23:42 +0200

    added new problem

--- src/CodeForces/dif_900/1555_A.py
+++ src/CodeForces/dif_900/1555_A.py
@@ -1,3 +1,28 @@
-from utils import readFile
+import sys
 
-l = readFile("C:\\Users\\Chris\\VSCodeProjects\\compPro\\src\\CodeForces\\dif_900\\input.txt")
+for k in range(int(sys.stdin.readline())):
+    n = int(sys.stdin.readline())
+
+    s = 0
+    m = 0
+    l = 0
+    
+    if n < 6:
+        print(15)
+        continue
+
+    r = n % 6
+    s += int(n / 6)
+
+    if r == 5:
+        s += 1
+    elif r == 1 or r == 2:
+        m += 1
+        s -= 1
+    elif r == 3 or r == 4:
+        l += 1
+        s -= 1
+
+    t = s * 15 + m * 20 + l * 25
+    print(t)
+        
